411cd05f5e126a3589b505f23d45c1daddfcf302
MMendes24 <mmbusiness1@protonmail.com>
2020-08-14 10:34:39 -0400

    Dummy reducer created, provider, store, and middleware setup, basic header rendering. Time for the axios get request.

---
+++ smurfs/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>Wow look a header!</div>
+    )
+}
+
+export default Header
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    // local data
+    isLoading: false,
+    error: "",
+
+    //from API
+    data: "",
+}
+
+const smurfReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default smurfReducer
